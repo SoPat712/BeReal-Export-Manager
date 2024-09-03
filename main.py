52357cfd96254a531dc4a78e5840062d671ddbc1
lmluk <lm.lukas@pm.me>
2024-09-03 15:04:00 +0200

    changed print_memories_info() to print_memory_info()

--- main.py
+++ main.py
@@ -7,18 +7,27 @@ def get_img_filename(image: json):
 def get_memory_location(memory: json):
   return memory['location'] if 'location' in memory else 'N/A'
 
-def print_memories_info(data: json):
-  n = 0
-  for i in data:
-    print("\nBeReal Nr. %s: " % n)
-    print(" - Time Taken: %s\n - Front Image: %s\n - Back Image: %s\n - Location: %s"
-           % (i['takenTime'],
-           get_img_filename(i['frontImage']), get_img_filename(i['backImage']),
-           get_memory_location(i)))
-    n+=1
+def print_memory_info(memory: json):
+  """
+  Takes a memory object from the ``memories.json`` file and prints out all necessarily information like:
+   - names from the images
+   - the taken time
+   - the location if available
+  """
+  print(" - BeReal Moment: %s\n - Time Taken: %s\n - Front Image: %s\n - Back Image: %s\n - Location: %s\n - isLate: %s"
+         % (memory['berealMoment'],
+            memory['takenTime'],
+            get_img_filename(memory['frontImage']), get_img_filename(memory['backImage']),
+            get_memory_location(memory),
+            memory['isLate']))
   
 
 if __name__ == '__main__':
   f = open('memories.json')
-  print_memories_info(json.load(f))
+  n = 0
+  for i in json.load(f):
+    print("\nBeReal Nr. %s: " % n)
+    print_memory_info(i)
+    n+=1
+    
   f.close()
